c9482bd27a01681e19ef52a245481cc08d8dd323
Andrey <999nekrol999@mail.ru>
2020-02-27 16:22:58 +0300

    1 commit

---
+++ Cherepenko Laba1 zadacha 1 var6.cpp
@@ -0,0 +1,58 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <stdio.h>
+#include <fstream>
+using namespace std;
+void prim_vb(int a[], int n);
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int* a;
+	int i = 0, n, k = 0;
+	ifstream abc;
+	abc.open("1.txt");//количество ненулевых элементов
+	while (!abc.eof())
+	{
+		abc >> n;
+		if (n > 0)
+		{
+			k++;
+		}
+	}
+	abc.close();
+	a = new int[k]; // динамический массив
+	abc.open("1.txt");
+	while (i < k)
+	{
+		abc >> n;//ввод из массива в файл новую переменную для проверки ненулевого элемента
+		if (n > 0)
+		{
+			a[i] = n; i++;
+		}
+	}
+	abc.close();
+	prim_vb(a, k);
+	for (int z = 0; z < k; z++)
+	{
+		cout << "a[" << z << "] = " << a[z] << endl;
+	}
+
+	return 0;
+}
+
+
+void prim_vb(int a[], int n)
+{
+	int sw = 0, min, i;
+	for (int j = 0; j < n; j++)
+	{
+		min = j; i = j + 1;
+		for (i; i < n; i++)
+		{
+			if (a[i] < a[min]) min = i;
+		}
+		sw = a[min];
+		a[min] = a[j];
+		a[j] = sw;
+	}
+}
